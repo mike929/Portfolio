a51cea59f73032d3d03b887d29550791a7b1d5f6
mike929 <mike929@gmail.com>
2019-01-25 21:04:16 -0500

    editing index page

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,7 +61,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "IonicaBizau");
+    GitHubCalendar("#github-graph", "mike929");
     
     
     /* Github Activity Feed - https://github.com/mike929/github-activity */
